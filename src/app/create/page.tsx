9c7afbcc02d8aa3b24a510ea8882d7bb09b094f2
Anshuman <107368860+anshuman008@users.noreply.github.com>
2024-08-04 15:56:12 +0530

    responsive design

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -6,14 +6,15 @@ import { useState } from "react";
 export default function Home() {
   const [shortUrl, setShortenUrl] = useState("");
   const [copySuccess, setCopySuccess] = useState(false);
-
+  const [loading,setLoading] = useState(false);
   const fetchApi = async (e:any) => {
     e.preventDefault();   
-           
+    setLoading(true);      
     const originalUrl = e.target.originalUrl.value;
 
     if(originalUrl){
           const response = await shortenURL(originalUrl);
+    setLoading(false);
     setShortenUrl(response);
     setCopySuccess(false);
     }
@@ -32,7 +33,9 @@ export default function Home() {
 
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#bdc3c7 ]to-[#2c3e50]">
+      <div className=" flex flex-col items-center justify-center bg-gradient-to-r from-[#bdc3c7 ]to-[#2c3e50]">
+
+        <div className="flex justify-center items-center h-[90vh] flex-col  ">
         <div className="p-10 bg-white rounded-lg shadow-2xl max-w-lg">
           <h1 className="text-3xl font-bold mb-6 text-center text-gray-700">
             URL SHORTY
@@ -59,7 +62,7 @@ export default function Home() {
         </div>
 
         {shortUrl && (
-          <div className="mt-6 p-4 bg-green-500 text-white rounded-lg flex items-center justify-between w-full max-w-lg">
+          <div className="mt-6 p-4 bg-green-500 px-3 overflow-hidden text-white rounded-lg flex items-center justify-between w-full max-w-lg flex-col gap-y-4 md:flex-row">
             <span>{`${process.env.NEXT_PUBLIC_BASE_URL}/api/shorten/${shortUrl}`}</span>
             <button
               onClick={copyToClipboard}
@@ -70,11 +73,18 @@ export default function Home() {
           </div>
         )}
 
+        
+   {loading && (
+        <span className="loading loading-infinity loading-lg mt-4 text-gray-200"></span>
+        )}
+
         {copySuccess && (
-          <div className="mt-2 text-center text-green-700">
+          <div className="mt-2 text-center text-green-700  ">
             Shortened URL copied to clipboard!
           </div>
         )}
+        </div>
+     
       </div>
    
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 const HeroComp = () => {
   return (
-         <div className="min-h-screen flex justify-start items-center bg-black flex-col pt-40">
+         <div className=" flex justify-center lg:justify-start items-center flex-col pt-56 lg:pt-40">
             <div className="flex flex-col gap-y-2">
-              <span className="text-7xl font-bold text-white">Short Links With</span>
-              <span className="text-7xl font-bold text-orange-400">Superpowers</span>
+              <span className="text-4xl md:text-7xl font-bold text-white">Short Links With</span>
+              <span className="text-4xl md:text-7xl font-bold text-orange-400">Superpowers</span>
             </div>
 
-            <span className="text-white my-6 text-2xl">Dub.co is the open-source link management infrastructure for modern marketing teams.</span>
+            <span className="text-white my-6 text-lg lg:text-2xl text-center">Shotii is the open-source link management infrastructure for modern marketing teams.</span>
 
                 <Link href={'/create'} className="btn btn-primary text-white">
                   Get Started
